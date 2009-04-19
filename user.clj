9ecabb108742fa68af00905678e983d9a3ed88db
David Edgar Liebke <liebke@gmail.com>
2009-04-18 22:01:55 -0400

    added user.clj that loads clojure.contrib.repl-utils

---
+++ user.clj
@@ -0,0 +1,8 @@
+(use '(clojure.contrib repl-utils))
+;(use '(incanter core stats io charts))
+
+;; the following line provides interupts
+;; to interupt long-running processes enter:
+;; ctl-c ctl-c \n
+(add-break-thread!)
+
