14a9999eccff5110b99a6828d079fbdf063e34e5
David Edgar Liebke <liebke@gmail.com>
2009-12-03 14:30:08 -0500

    removed geospatial_tests.clj

--- test/incanter/geospatial_tests.clj
+++
@@ -1,7 +0,0 @@
-(ns incanter.geospatial-tests
-  (:use clojure.contrib.test-is)
-  (:use incanter.geospatial))
-
-(deftest distance-tests
- (is ( = 0
-	 (distance-between [27.405328 68.607269] [27.405328 68.607269]))))
