79a912a21eb9de168bd36922c4684e647fcbb5ae
David Edgar Liebke <liebke@gmail.com>
2009-12-10 23:46:07 -0500

    updated project.clj files in examples to point to incanter-1.0-SNAPSHOT.jar

--- examples/blog/projects/incanter-helloworld/project.clj
+++ examples/blog/projects/incanter-helloworld/project.clj
@@ -1,5 +1,6 @@
 (defproject incanter-helloworld "0.1.0"
               :description "The Incanter version of hello world."
-              :dependencies [[incanter/incanter "0.9.0"]]
+              ;:dependencies [[incanter/incanter "0.9.0"]]
+              :dependencies [[incanter/incanter "1.0-SNAPSHOT"]]
               :main hello)
 
--- examples/blog/projects/simple_web_app/project.clj
+++ examples/blog/projects/simple_web_app/project.clj
@@ -1,7 +1,7 @@
 
 (defproject incanter-webapp "0.1.0"
   :description "A simple Incanter web application."
-  :dependencies [[incanter/incanter "0.9.0"]
+  :dependencies [[incanter/incanter "1.0-SNAPSHOT"]
                  [org.clojars.ato/compojure "0.3.1"]]
   :main simple_web_app)
 
