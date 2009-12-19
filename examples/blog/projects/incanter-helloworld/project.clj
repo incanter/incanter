65624ebb95486876b67dfb75408e39bb586aac53
David Edgar Liebke <liebke@gmail.com>
2009-12-19 14:31:05 -0500

    minor change to example project.clj file.

--- examples/blog/projects/incanter-helloworld/project.clj
+++ examples/blog/projects/incanter-helloworld/project.clj
@@ -1,5 +1,5 @@
 (defproject incanter-helloworld "0.1.0"
               :description "The Incanter version of hello world."
-              :dependencies [[incanter/incanter "1.0-master-SNAPSHOT"]]
+              :dependencies [[incanter "1.0-master-SNAPSHOT"]]
               :main hello)
 
