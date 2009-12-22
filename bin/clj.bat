6b3576f89507428f5bf86eb876918fbe2b29c131
David Edgar Liebke <liebke@gmail.com>
2009-12-21 20:43:53 -0500

    renamed incanter-*-jar-with-dependencies.jar to incanter-exec.jar, and added instructions on running Incanter directly with 'java -jar incanter-exec.jar'

--- bin/clj.bat
+++ bin/clj.bat
@@ -1,7 +1,7 @@
 @echo off
     set INCANTER_HOME=.
     set CLASSES_DIR=%INCANTER_HOME%\classes
-    set INCANTER_JAR=%INCANTER_HOME%\target\incanter-1.0-SNAPSHOT-jar-with-dependencies.jar
+    set INCANTER_JAR=%INCANTER_HOME%\target\incanter-exec.jar
 
     IF (%1)==() (
         java -cp %INCANTER_JAR% -Dclojure.compile.path=%CLASSES_DIR% -Dincanter.home=%INCANTER_HOME%  clojure.main
