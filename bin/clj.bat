45ffb5bd7005ccc0b7571227ce24b75824af0770
David Edgar Liebke <liebke@gmail.com>
2009-12-26 12:07:54 -0500

    added jline and clojure-swank to incanter-bundle dependencies

--- bin/clj.bat
+++ bin/clj.bat
@@ -1,7 +1,7 @@
 @echo off
     set INCANTER_HOME=.
     set CLASSES_DIR=%INCANTER_HOME%\classes
-    set INCANTER_JAR=%INCANTER_HOME%\target\incanter-exec.jar
+    set INCANTER_JAR=%INCANTER_HOME%\incanter-bundle\target\incanter-exec.jar
 
     IF (%1)==() (
         java -cp %INCANTER_JAR% -Dclojure.compile.path=%CLASSES_DIR% -Dincanter.home=%INCANTER_HOME%  clojure.main
