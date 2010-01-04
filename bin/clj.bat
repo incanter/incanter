9dfba50a94c08e273eadbd02f772ca5656c605d1
Alex Ott <alexott@gmail.com>
2010-01-04 19:59:32 +0100

    fix scripts to use new bundle names
    bin/clj & bin/swank should also be called from any place, not only from incanter top-level directory

--- bin/clj.bat
+++ bin/clj.bat
@@ -1,7 +1,7 @@
 @echo off
     set INCANTER_HOME=.
     set CLASSES_DIR=%INCANTER_HOME%\classes
-    set INCANTER_JAR=%INCANTER_HOME%\modules\incanter-bundle\target\incanter-exec.jar
+    set INCANTER_JAR=%INCANTER_HOME%\modules\incanter-app\target\incanter-app-1.0-master-SNAPSHOT.jar
 
     IF (%1)==() (
         java -cp %INCANTER_JAR% -Dclojure.compile.path=%CLASSES_DIR% -Dincanter.home=%INCANTER_HOME%  clojure.main
