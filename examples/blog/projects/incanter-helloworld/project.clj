1410f1e25fff5cb60a4bf9b7223781ca2566e0a1
David Edgar Liebke <liebke@gmail.com>
2010-01-08 11:46:39 -0500

    trying to resolve problem with clojars version of incanter.

--- examples/blog/projects/incanter-helloworld/project.clj
+++ examples/blog/projects/incanter-helloworld/project.clj
@@ -1,5 +1,5 @@
 (defproject incanter-helloworld "0.1.0"
               :description "The Incanter version of hello world."
-              :dependencies [[incanter "1.0-master-SNAPSHOT"]]
+              :dependencies [[incanter/incanter-full "1.0-master-SNAPSHOT"]]
               :main hello)
 
