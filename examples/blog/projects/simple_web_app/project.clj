1637b60bf06f49cecfc1a0a3189b8bd51ee61fb5
David Edgar Liebke <liebke@gmail.com>
2010-02-22 20:00:03 -0500

    Updated simple_web_app example.

--- examples/blog/projects/simple_web_app/project.clj
+++ examples/blog/projects/simple_web_app/project.clj
@@ -1,7 +1,8 @@
 
 (defproject incanter-webapp "0.1.0"
   :description "A simple Incanter web application."
-  :dependencies [[incanter "1.0-SNAPSHOT"]
+  :repositories {"incanter" "http://repo.incanter.org"}
+  :dependencies [[org.incanter/incanter-full "1.0.0"]
                  [org.clojars.liebke/compojure "0.3.1-master"]]
   :main simple_web_app)
 
