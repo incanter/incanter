ed37669d1014302214ce4ea9e27270e8729a604b
David Edgar Liebke <liebke@gmail.com>
2010-03-31 11:23:49 -0400

    More updates to example project.clj files.

--- examples/blog/projects/string_template/project.clj
+++ examples/blog/projects/string_template/project.clj
@@ -1,8 +1,5 @@
 (defproject myproject "1.0.0-SNAPSHOT" 
               :description "Example Incanter project" 
-              :repositories {"incanter" "http://repo.incanter.org"}
-              :dependencies [[org.incanter/incanter-full "1.0.0"]
-              ;:dependencies [[org.incanter/incanter-full "1.2.0-SNAPSHOT"]
+              :dependencies [[incanter "1.2.1-SNAPSHOT"]
                              [antlr/stringtemplate "2.2"]]
-              ;:dev-dependencies [[leiningen/lein-swank "1.1.0"]]
               :main hello)
--- examples/blog/projects/swank/project.clj
+++ examples/blog/projects/swank/project.clj
@@ -1,5 +1,5 @@
 (defproject incanter-swank "0.1.0"
   :description "A Swank Server for Incanter"
-  :dependencies [[incanter "1.0-master-SNAPSHOT"]]
+  :dependencies [[incanter "1.2.1-SNAPSHOT"]]
   :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]])
 
