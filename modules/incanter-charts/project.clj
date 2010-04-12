54d9843d8759b9e489283ff8b73aebaf14b99af4
David Edgar Liebke <liebke@gmail.com>
2010-04-13 03:46:36 +0800

    Changed version number to 1.2.1.

--- modules/incanter-charts/project.clj
+++ modules/incanter-charts/project.clj
@@ -1,7 +1,7 @@
-(defproject incanter/incanter-charts "1.2.1-SNAPSHOT"
+(defproject incanter/incanter-charts "1.2.1"
   :description "Incanter-charts is the JFreeChart module of the Incanter project."
-  :dependencies [[incanter/incanter-core "1.2.1-SNAPSHOT"]
+  :dependencies [[incanter/incanter-core "1.2.1"]
                  [incanter/jfreechart "1.0.13"]]
-  :dev-dependencies [[incanter/incanter-io "1.2.1-SNAPSHOT"]
+  :dev-dependencies [[incanter/incanter-io "1.2.1"]
                      [lein-clojars "0.5.0-SNAPSHOT"]
                      [leiningen/lein-swank "1.1.0"]])
--- modules/incanter-io/project.clj
+++ modules/incanter-io/project.clj
@@ -1,6 +1,6 @@
-(defproject incanter/incanter-io "1.2.1-SNAPSHOT"
+(defproject incanter/incanter-io "1.2.1"
   :description "Incanter-io is the I/O module of the Incanter project."
-  :dependencies [[incanter/incanter-core "1.2.1-SNAPSHOT"]
+  :dependencies [[incanter/incanter-core "1.2.1"]
                  [net.sf.opencsv/opencsv "2.0.1"]
                  [org.danlarkin/clojure-json "1.1-SNAPSHOT"
                    :exclusions [org.clojure/clojure org.clojure/clojure-contrib]]]
