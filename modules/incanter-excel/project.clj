cb9d8d13f7a5f629beb2df26b54bfdcd1edb2716
David James Humphreys <davidjameshumphreys@gmail.com>
2010-05-19 23:46:05 +0100

    Fix the excel project settings
    Update the versions to match other projects.

--- modules/incanter-excel/project.clj
+++ modules/incanter-excel/project.clj
@@ -1,7 +1,6 @@
-(defproject incanter/incanter-excel "1.2.1"
-  :description "Incanter-excel provides simple access to writing Excel files."
-  :dependencies [[incanter/incanter-core "1.2.1"]
+(defproject incanter/incanter-excel "1.2.3-SNAPSHOT"
+  :description "Incanter-excel provides access to reading and writing Excel files."
+  :dependencies [[incanter/incanter-core "1.2.3-SNAPSHOT"]
   		 [org.apache.poi/poi "3.6"]]
-  :dev-dependencies [[incanter/incanter-io "1.2.1"]
-                     [lein-clojars "0.5.0-SNAPSHOT"]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [leiningen/lein-swank "1.1.0"]])
