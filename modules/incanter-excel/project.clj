73bf736dbd4a5d9acbbc1b82248e5154a750ae91
David Edgar Liebke <liebke@gmail.com>
2010-05-29 22:25:37 +0800

    Removed lein-swank dev-dependencies from submodules.

--- modules/incanter-excel/project.clj
+++ modules/incanter-excel/project.clj
@@ -2,5 +2,4 @@
   :description "Incanter-excel provides access to reading and writing Excel files."
   :dependencies [[incanter/incanter-core "1.2.3-SNAPSHOT"]
   		 [org.apache.poi/poi "3.6"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
-                     [leiningen/lein-swank "1.1.0"]])
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
