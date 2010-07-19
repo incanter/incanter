9e65d98dc9076743f6483f799c499b9ee9084798
David Edgar Liebke <liebke@gmail.com>
2010-07-19 13:46:11 -0400

    Updated to clojure and clojure-contrib 1.2.0-beta1.

--- modules/incanter-core/project.clj
+++ modules/incanter-core/project.clj
@@ -1,10 +1,8 @@
 (defproject incanter/incanter-core "1.2.3-SNAPSHOT"
   :description "Incanter-core is the core module of the Incanter project."
   :dependencies [
-                 [org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 ;[org.clojure/clojure "1.2.0-master-20100426.160114-46"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-                 ;[org.clojure/clojure-contrib "1.2.0-20100427.200505-82"]
+                 [org.clojure/clojure "1.2.0-beta1"]
+                 [org.clojure/clojure-contrib "1.2.0-beta1"]
                  [incanter/parallelcolt "0.9.4"]]
   :dev-dependencies [[lein-javac "0.0.2-SNAPSHOT"]
                      [lein-clojars "0.5.0-SNAPSHOT"]]
