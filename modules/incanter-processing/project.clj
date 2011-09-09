13e0504348d71c713b4a800fc5b994661b3db24d
Michael Nygard <mtnygard@gmail.com>
2011-09-09 16:02:27 -0500

    incanter-processing passes tests on Clojure 1.3.0-beta3

--- modules/incanter-processing/project.clj
+++ modules/incanter-processing/project.clj
@@ -1,5 +1,5 @@
-(defproject incanter/incanter-processing "1.2.3"
+(defproject incanter/incanter-processing "1.3.0-SNAPSHOT"
   :description "Incanter-processing is the Processing module of the Incanter project."
-  :dependencies [[incanter/incanter-core "1.2.3"]
+  :dependencies [[incanter/incanter-core "1.3.0-SNAPSHOT"]
                  [incanter/processing-core "1.1"]]
   :dev-dependencies [[lein-clojars "0.6.0"]])
