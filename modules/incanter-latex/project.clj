e0309007aec6ac2c4085abca3ffbfa9dad7a54dd
David James Humphreys <davidjameshumphreys@gmail.com>
2011-09-18 22:35:33 +0100

    Added a 'to-latex' function converts an incanter matrix to a LaTeX string.

--- modules/incanter-latex/project.clj
+++ modules/incanter-latex/project.clj
@@ -1,6 +1,7 @@
 (defproject incanter/incanter-latex "1.2.3"
   :description "Library for rendering LaTeX math equations using the jLateXMath library."
   :dependencies [[incanter/incanter-charts "1.2.3"]
+                 [incanter/incanter-core "1.2.3"]
                  [net.sf.alxa/jlatexmath "0.9.1-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
                      [lein-clojars "0.6.0"]]
