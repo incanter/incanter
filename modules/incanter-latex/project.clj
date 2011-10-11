2f458c9992d2484ca5bafd8aa2aa69bd21b660e5
David Edgar Liebke <liebke@gmail.com>
2011-10-11 16:40:06 -0400

    Added lein-clojars as incanter-latex dev-dependency

--- modules/incanter-latex/project.clj
+++ modules/incanter-latex/project.clj
@@ -2,4 +2,5 @@
   :description "Library for rendering LaTeX math equations using the jLateXMath library."
   :dependencies [[incanter/incanter-charts "1.3.0-SNAPSHOT"]
                  [net.sf.alxa/jlatexmath "0.9.1-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.7.0"]]
   :repositories {"alxa-repo" "http://alxa.sourceforge.net/m2"})
