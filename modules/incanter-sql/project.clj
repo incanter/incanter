5fe3359d3dc89c6893e51c2f233cc0752af3de70
David James Humphreys <davidjameshumphreys@gmail.com>
2011-10-23 15:18:12 +0100

    Incanter & ClojureQL integration.
    Basic support to convert ClojureQL queries into Incanter Datasets.
    
    * 'read-dataset' dereferences a clojureql statement and builds the dataset lazily.
    * 'insert-dataset' inserts the data into the given table name.
    
    Signed-off-by: Alex Ott <alexott@gmail.com>

---
+++ modules/incanter-sql/project.clj
@@ -0,0 +1,6 @@
+(defproject incanter-sql "1.2.3"
+  :description "Database interaction via ClojureQL"
+  :dependencies [[incanter/incanter-core "1.2.3"]
+                 [org.clojure/clojure "1.2.1"]
+                 [clojureql "1.1.0-SNAPSHOT"]]
+  :dev-dependencies [[org.apache.derby/derby "10.8.1.2"]])
