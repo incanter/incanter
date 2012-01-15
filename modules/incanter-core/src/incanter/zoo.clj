ce3df8051a914dc4f5c6d29e55298dcc8c225fc4
Paul Lam <paul.lam@forward.co.uk>
2012-01-15 13:04:54 +0000

    pass simple moving average tests

--- modules/incanter-core/src/incanter/zoo.clj
+++ modules/incanter-core/src/incanter/zoo.clj
@@ -31,4 +31,39 @@
                               gamma pow sqrt diag trans regularized-beta ncol
                               nrow identity-matrix decomp-cholesky decomp-svd
                               matrix length log10 sum sum-of-squares sel matrix?
-                              cumulative-sum solve vectorize bind-rows)]))
+                              cumulative-sum solve vectorize bind-rows)]
+        [incanter.stats :only (mean)]))
+
+;; Credit: http://www.learningclojure.com/2010/03/moving-average-of-list.html
+(defn partialsums [start coll]
+  (lazy-seq
+    (if-let [coll (seq coll)]
+          (cons start (partialsums (+ start (first coll)) (rest coll)))
+          (list start))))
+
+(defn sliding-window-sma [n coll]
+  (map #(/ % n)
+       (let [start   (apply + (take n coll))
+             diffseq (map   - (drop n coll) coll)]
+         (partialsums start diffseq))))
+
+(defn partition-sma [n coll]
+  (map mean (partition n 1 coll)))
+
+(defn rollmean
+" 
+  Returns the unweighted mean of the previous n data points.
+  
+  Arguments:
+  :small-n?  Algorithm is optimised for either small n (e.g. n < 20) 
+             or large n (e.g. n > 20). Default is true.
+
+  References: 
+  http://en.wikipedia.org/wiki/Moving_average#Simple_moving_average
+"  
+  [n coll & {:keys [small-n?]
+             :or   [small-n? true]}]
+  (if :small-n?
+    (partition-sma n coll)
+    (sliding-window-sma n coll)))
+
--- modules/incanter-core/test/incanter/zoo_test.clj
+++ modules/incanter-core/test/incanter/zoo_test.clj
@@ -2,3 +2,13 @@
   (:use clojure.test 
         (incanter zoo core stats)))
 
+(def integers (iterate inc 0))
+(def sma-5 [2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0 11.0])
+
+(deftest simple-moving-average-test
+  (is (= sma-5
+         (take 10  (simple-moving-average 5 integers))))
+  (is (= [2 3 4 5 6 7 8 9 10 11]
+         (take 10 (map int (partition-sma 5 integers)))))
+  (is (= [2 3 4 5 6 7 8 9 10 11]
+         (take 10 (sliding-window-sma 5 integers)))))
