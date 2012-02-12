f8ec625b2d085ac1a5f486ec97c997e56ff92505
esj <edmundsjackson@gmail.com>
2012-02-12 12:50:24 +0000

    Added tests for all external facing functions.

--- modules/incanter-zoo/src/incanter/zoo.clj
+++ modules/incanter-zoo/src/incanter/zoo.clj
@@ -24,7 +24,6 @@
              "
         :author "David Edgar Liebke"}
    incanter.zoo
-   (:refer clojure :exclude [sort])
    (:import (cern.colt.list.tdouble DoubleArrayList))
    (:use incanter.backstage.zoo-commons
          [incanter.core :only ($ abs plus minus div mult mmult to-list bind-columns
@@ -108,17 +107,52 @@
   [seq elm]  
   (some #(= elm %) seq))
 
-;; Single protocol that convert a value to a Joda value
+;; Single protocol that convert a value to a Joda value.  These changes
+;; have now been merged upsteam in sean-corfield/clj-time, so we'll not have to do it ourselves
+;; once he releases.
 (defprotocol JodaCoercible
-  (to-clj-time [x]))
+  (to-date-time [x]))
 
 (extend-protocol JodaCoercible
   java.lang.String
-  (to-clj-time [x] (c/from-string x))
+  (to-date-time [x] (c/from-string x))
   org.joda.time.DateTime
-  (to-clj-time [x] x)
+  (to-date-time [x] x)
   java.lang.Long
-  (to-clj-time [x] (c/from-long x)))
+  (to-date-time [x] (c/from-long x)))
+
+(defn aligned?
+  "Is the :index column identical for all zs."
+  [& zs]
+  (and
+   ;; All have the same length
+   (->> zs
+        (map nrow)
+        (apply =))
+   ;; Every index is identical
+   (->> zs
+        (map (partial $ :index))
+        (apply map =)
+        (every? identity))))
+
+(defn- row-order
+  "Orders a seq of rows by index"
+  [z]
+  (->> z
+      (apply sorted-set-by #(compare (:index %1) (:index %2)))
+      (into [])))
+
+(defn- order
+  "Order a zoo so that the :index is increasing in time."
+  [z]
+  (update-in z [:rows] row-order))
+
+(defn within-zoo?
+  "Is t between the first and last indices."
+  [t z]
+  (t/within? 
+   (t/interval ($ 0 :index z)  ($ (dec (nrow z)) :index z))
+   (to-date-time t)))
 
 (defn zoo
   "Return the given dataset as a zoo value which is simply a dataset
@@ -127,34 +161,37 @@ That column must contain values that can be coerced into Jodas using the TimeCoe
   ([x] (zoo x :index))
   ([x index-col]
      {:pre [(-> x :column-names (in? index-col))]}
-     (to-dataset
-      (conj-cols
-       (map (fn [{i index-col :as v}]
-              (-> v
-                  (dissoc index-col)
-                  (assoc :index (to-clj-time i))))
-            (:rows x))))))
+     (order
+      (to-dataset
+       (conj-cols
+        (map (fn [{i index-col :as v}]
+               (-> v
+                   (dissoc index-col)
+                   (assoc :index (to-date-time i))))
+             (:rows x)))))))
 
+;; I suspect this could be far better implemented using sel or such.
 (defn $$
-  "This is the equivalent of :: in xts.  That is, it slices
- out the timeseries between ind-1 and ind-2.  These are any values that
- can be coerced into clj-time values. No column selection is supported"
+  "This is the equivalent of :: in xts.  That is, it slices  out the timeseries between ind-1 and ind-2.  These are any values that can be coerced into clj-time values."
   ([ind ts]
-     ($where {:index (to-clj-time ind)} ts))
-  ([ind-1 ind-2 ts]
-     ($where (fn [row]
-               (let [i1 (to-clj-time ind-1)
-                     i2 (to-clj-time ind-2)]
-                   ;; Extend by 1 milli, to close interval
-                   (let [interval (t/extend
-                                   (t/interval i1 i2)
-                                   (t/millis 1))]
-                     (t/within? interval (row :index)))))
-             ts)))
+     ($ :all ($where {:index (to-date-time ind)} ts)))
+  ([ind cols ts]
+     ($ cols ($where {:index (to-date-time ind)} ts)))
+  ([ind-1 ind-2 cols ts]
+     ($ cols
+        ($where (fn [row]
+                  (let [i1 (to-date-time ind-1)
+                        i2 (to-date-time ind-2)]
+                    ;; Extend by 1 milli, to close interval
+                    (let [interval (t/extend
+                                    (t/interval i1 i2)
+                                         (t/millis 1))]
+                      (t/within? interval (row :index)))))
+                ts))))
 
 (defn- nil-row
   "Returns a map with the same keys as x, but with nils for
-each value.  Used for padding zoo."
+each value.  Used for padding zoo in functions that shorten it."
   [x]
   (zipmap (keys x) (repeat nil)))
 
@@ -184,17 +221,8 @@ each value.  Used for padding zoo."
             (roll-apply f n ($ column zoo))))
    [:index column]))
 
-(defn aligned?
-  "Is the :index column identical for all zs."
-  [& zs]
-  (->> zs
-       (map (partial $ :index))
-       (apply map =)
-       (every? identity)))
-
 (defn zoo-row-map-
-  "Accept a number of aligned zoo object and pass them row-wise into f, return a seq
-of maps of the output of the output. f must accept and return maps.  The :index column is stripped out before f is applied, and then replaced afterwards."
+  "Accept a number of aligned zoo object and pass them row-wise into f, return a seq of maps of the output of the output. f must accept and return maps.  The :index column is stripped out before f is applied, and then replaced afterwards."
   [f & zs]
   {:pre [(apply aligned? zs)]}
   (->> zs
@@ -219,87 +247,19 @@ of maps of the output of the output. f must accept and return maps.  The :index 
        (apply zoo-row-map- f)
        (remove index-only?)))
 
-(defn- row-sort
-  "Sorts a seq of rows by index"
-  [z]
-  (-> z
-      (apply sorted-set-by #(compare (:index %1) (:index %2)))
-      (into [])))
-
-(defn sort
-  "Sort a zoo so that the index increasing in time."
-  [z]
-  (update-in z [:rows] row-sort))
-
-(defn within-zoo?
-  "Is t between the first and last indices."
-  [t z]
-  (t/within? 
-   (t/interval (-> z :rows first :index)
-               (-> z :rows last :index))
-   t))
-
+  
 (comment
-  "This is just here for now to demo the zoo functions."
-  ;; First create a normal dataset
-  (def ds1 (to-dataset [{:index "2012-01-01" :temp 32 :press 100}
-                        {:index "2012-01-02" :temp 35 :press 98}
-                        {:index "2012-01-03" :temp 30 :press 102}
-                        {:index "2012-01-04" :temp 31 :press 103}
-                        {:index "2012-01-05" :temp 32 :press 104}
-                        {:index "2012-01-06" :temp 33 :press 105}]))
 
-  (def ds2 (to-dataset [{:date "2012-01-01" :temp 32 :press 100}
-                        {:date "2012-01-02" :temp 35 :press 98}
-                        {:date "2012-01-03" :temp 30 :press 102}
-                        {:date "2012-01-04" :temp 31 :press 103}
-                        {:date "2012-01-05" :temp 32 :press 104}
-                        {:date "2012-01-06" :temp 33 :press 105}]))
-  
-  ;; Turn it into a zoo.  Clearly we should just mirror the to-dataset
-  ;; with a to-zoo function
-  (def ts1 (zoo ds1))
-  (def ts2 (zoo ds2 :date))
-
-  ;; Slice out easily
-  ($$ "2012-01-01" ts1)
-  ($$ "2012-01-01" "2012-01-03" ts1)
-  ($$ "2012-01-01" (c/from-string "2012-01-03") ts1)  ;; Cool !
-  
-
-  ;; Lag easily.  Clearly there should be a k, maybe with negatives for lead.
-  (lag ts1)
-  (lag ts1 2)
-
-  ;; ordering
-  
-  ;; Demo zoo-apply
-  (zoo-apply #(apply min %) 2 ts1 :temp)
-
-  ;; Demo of aligned?
-  (aligned? ts1 (lag ts1))
-  
-  ;; Demo zoo-row-map
-  ;; First a map consuming function
-  (defn map-diff
-    "Return a map with the different values of v2 - v1"
-    [m1 m2]
-    (into {} (map (fn [[k1 v1] [k2 v2]]
-                    (if (and v1 v2)
-                      {k2 (- v1 v2)}
-                      {k2 nil}))
-                  m1 m2)))
-        
-  ;; Now use it
-  (zoo-row-map- map-diff ts1 ts1)
-  (zoo-row-map map-diff ts1 ts1)
-  (zoo-row-map map-diff ts1 (lag ts1))
-  
-  ;; Questions
-  ;; Think of general nil handling.  Feels like a maybe monad here.
-  ;; Should it be called roll-map not roll-apply to conform to Clojure rather than R naming ?
-  ;; Should roll-apply perhaps has (apply f args) etc internall ?
-
-  )
+  ;; This is for thinking about at the core level. Not here.
+  (defn fill-missing
+  "Return a dataset with previously missing values replaced with a default value."
+  [d v]
+  (to-dataset
+   (map merge 
+        (-> d
+            :column-names
+            (zipmap (repeat v))
+            repeat)
+        (:rows d)))))
 
 
--- modules/incanter-zoo/test/incanter/zoo_test.clj
+++ modules/incanter-zoo/test/incanter/zoo_test.clj
@@ -1,7 +1,10 @@
 (ns incanter.zoo-test
   (:use clojure.test 
-        (incanter zoo core stats)))
+        (incanter zoo core stats))
+  (:require [clj-time.coerce :as c]))
 
+
+;; --= Tests
 (def integers (iterate inc 0))
 
 (deftest roll-apply-test
@@ -23,3 +26,131 @@
 (deftest roll-min-test
   (is (= [0 1 2 3 4]
          (take 5 (roll-min 3 integers)))))
+
+;; --------------------
+;; Standard dataset, with :index column
+(def ds1 (to-dataset [{:index "2012-01-01" :temp 32 :press 100}
+                      {:index "2012-01-02" :temp 35 :press 98}
+                      {:index "2012-01-03" :temp 30 :press 102}
+                      {:index "2012-01-04" :temp 31 :press 103}
+                      {:index "2012-01-05" :temp 32 :press 104}
+                      {:index "2012-01-06" :temp 33 :press 105}]))
+
+;; Standard dataset with :date column
+(def ds2 (to-dataset [{:date "2012-01-01" :temp 32 :press 100}
+                      {:date "2012-01-02" :temp 35 :press 98}
+                      {:date "2012-01-03" :temp 30 :press 102}
+                      {:date "2012-01-04" :temp 31 :press 103}
+                      {:date "2012-01-05" :temp 32 :press 104}
+                      {:date "2012-01-06" :temp 33 :press 105}]))
+
+;; Data with missing values
+(def ds3 (to-dataset [{:date "2012-01-01" :temp 32}
+                      {:date "2012-01-02" :press 98 :temp 32}
+                      {:date "2012-01-06" :temp 33}]))
+
+(deftest zoo-test
+  (is (zoo ds1))
+  (is (zoo ds2 :date))
+  (is (zoo ds3 :date))
+  (testing "Check ordering"
+    (let [ts (zoo (to-dataset [{:index "2012-01-01" :a 1}
+                               {:index "2012-01-02" :a 2}
+                               {:index "2012-01-03" :a 3}]))]
+      (is (= ts
+             (zoo (to-dataset [{:index "2012-01-01" :a 1}
+                               {:index "2012-01-03" :a 3}
+                               {:index "2012-01-02" :a 2}]))))
+      (is (= ts
+             (zoo (to-dataset [{:index "2012-01-03" :a 3}
+                               {:index "2012-01-02" :a 2}
+                               {:index "2012-01-01" :a 1}])))))))
+
+(deftest $$-test
+  ;; Time slicing
+  (let [ts1 (zoo ds1)]
+    (testing "Single date slice"
+      (is (= ($$ "2012-01-01" :temp ts1) 32) "Single col")
+      (is (= ($$ "2012-01-01" :all ts1)
+             [100 32 (to-date-time "2012-01-01")]) "Whole row")
+      (is (= ($$ "2012-01-01" :all ts1)
+             ($$ "2012-01-01" ts1)) "Single arity check")
+      (is (= (nrow ($$ "2012-01-10" :all ts1)) 0) "Date out of range"))
+
+    (testing "Date range slice"
+      (let [slice ($$ "2012-01-01" "2012-01-03" :all ts1)]
+        (is (= 3 (nrow slice)))
+        (is (= 3 (ncol slice))))
+      (is (= ($$ "2012-01-01" "2012-01-03" :temp ts1) [32 35 30]) "Single col"))
+
+    (testing "Native Joda as index"
+      (is (= ($$ (c/from-string "2012-01-03") :temp ts1) 30)))
+    
+    (testing "End point overlaps"
+      (is (= ($$ "2012-01-01" "2012-01-06" :all ts1)
+             ($$ "2012-01-01" "2012-06-10" :all ts1)) "RHS")
+      (is (= ($$ "2012-01-01" "2012-01-06" :all ts1)
+             ($$ "2011-01-01" "2012-06-10" :all ts1)) "LHS&RHS"))))
+
+(deftest aligned?-test
+  (let [ts1 (zoo ds1)
+        ts2 (zoo ds2 :date)]
+    (aligned? ts1 ts2)
+    (is (not (aligned? ts1
+                       (zoo (to-dataset [{:index "2012-01-01" :a 1}
+                                         {:index "2012-01-02" :a 2}]))))
+        "Unequal length")
+    (is (not (aligned? (zoo (to-dataset [{:index "2012-01-01" :a 1}
+                                         {:index "2012-01-02" :a 2}]))
+                       (zoo (to-dataset [{:index "2012-01-02" :a 1}
+                                         {:index "2012-01-03" :a 2}]))))
+        "Different indices")))
+
+(deftest within-zoo?-test
+  (let [ts1 (zoo ds1)]
+    (is (within-zoo? "2012-01-01" ts1))
+    (is (within-zoo? "2012-01-04" ts1))
+    (is (not (within-zoo? "2012-01-11" ts1)))
+    (is (within-zoo? (c/from-string "2012-01-02T01:03:03") ts1) "Needn't be same frequency")))
+
+(deftest lag-test
+  (let [ts1 (zoo ds1)
+        ls1 (lag ts1)]
+    (is (aligned? ts1 ls1) "Indices equal")
+    (is (aligned? ts1 (lag ls1)) "Indices equal")
+    (is (aligned? ts1 (lag ts1 3)) "Indices equal")
+    
+    (is (= ($ 0 [:press :temp] ls1)
+           [nil nil]) "Nil pad the front values")
+    (is (= ($ 0 [:press :temp] ts1)
+           ($ 1 [:press :temp] ls1)) "Second vals of lag = first of ts")
+    (is (= ($ 0 [:press :temp] ts1)
+           ($ 2 [:press :temp] (lag ts1 2))) "Multiperiod lag")
+    (is (= ($ 2 [:press :temp] (-> ts1 lag lag))
+           ($ 2 [:press :temp] (lag ts1 2))) "Multiperiod lag")))
+
+(deftest zoo-apply-test
+  (let [ts (zoo ds1)
+        zs (zoo-apply #(apply min %) 2 ts :temp)]
+    (aligned? ts zs)
+    (is (= (rest ($ :temp zs))
+           (roll-min 2 ($ :temp ts))))))
+
+;; Helper for test below
+(defn- map-diff
+  "Return a map with the different values of v2 - v1"
+  [m1 m2]
+  (into {} (map (fn [[k1 v1] [k2 v2]]
+                  (if (and v1 v2)
+                    {k2 (- v1 v2)}
+                    {k2 nil}))
+                m1 m2)))
+
+(deftest zoo-row-map-test
+  (let [ts1 (zoo ds1)
+        ms1 (zoo-row-map map-diff ts1 ts1)
+        ms2 (zoo-row-map map-diff ts1 (lag ts1))]
+    (is (every? (partial = 0) ($ :temp ms1)))
+    ms2
+    (is (= ($ :press ms2) [nil -2 4 1 1 1]))))
+    
