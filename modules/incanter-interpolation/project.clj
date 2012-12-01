bdd87d694ce967c69d2dda86d24cc3512a3acb69
Nikita Beloglazov <nikelandjelo@gmail.com>
2012-12-01 23:16:46 +0300

    Add linear interpolation for 1-argument functions.

--- modules/incanter-interpolation/project.clj
+++ modules/incanter-interpolation/project.clj
@@ -6,4 +6,5 @@
   :scm {:name "git" :url "https://github.com/liebke/incanter"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [incanter/incanter-core "1.4.0"]])
+                 [incanter/incanter-core "1.4.0"]
+                 [incanter/incanter-charts "1.4.0"]])
--- modules/incanter-interpolation/src/incanter/interpolation.clj
+++ modules/incanter-interpolation/src/incanter/interpolation.clj
@@ -1,5 +1,48 @@
 (ns incanter.interpolation)
 
-(defn interpolate [xs ys]
-  (fn [x] x))
+(defn out-of-range [x points]
+  (throw (IllegalArgumentException.
+          (format "x = %s is out of range [%s, %s]"
+                  x
+                  (apply min (map first points))
+                  (apply max (map first points))))))
 
+(defn interpolate-linear [points]
+  (let [pairs (partition 2 1 points)]
+    (fn [x]
+      (if-let [interv (->> pairs
+                           (filter (fn [[[xl] [xr]]] (<= xl x xr)))
+                           first)]
+        (let [[[xl yl] [xr yr]] interv
+              coef (/ (- x xl) (- xr xl))
+              calc (fn [yl yr] (+ (* (- 1 coef) yl)
+                                  (* coef yr)))]
+          (if (coll? yl)
+            (map calc yl yr)
+            (calc yl yr)))
+        (out-of-range x points)))))
+
+(defn interpolate [xs ys]
+  (->> (map vector xs ys)
+       (sort-by first)
+       interpolate-linear))
+
+
+
+#_(
+
+   ((interpolate [0 2 1] [[0 1] [2 3] [4 5]]) 1.5)
+
+   (require '[incanter.core :as core])
+   (require '[incanter.charts :as charts])
+
+   (let [n 10
+         xs (repeatedly n #(rand-int 10))
+         ys (repeatedly n #(rand-int 10))
+         min-x (apply min xs)
+         max-x (apply max xs)
+         f (interpolate xs ys)]
+     (core/view (charts/function-plot f min-x max-x)))
+
+
+   )
