2e504c870ea0e635a8b6f87dafa0200215a08888
Alex Ott <alexott@gmail.com>
2012-12-16 17:46:29 +0100

    fix project's name

--- modules/incanter-sql/project.clj
+++ modules/incanter-sql/project.clj
@@ -1,4 +1,4 @@
-(defproject incanter-sql "1.4.1"
+(defproject incanter/incanter-sql "1.4.1"
   :description "Database interaction via ClojureQL"
   :dependencies [[incanter/incanter-core "1.4.1"]
                  [clojureql "1.0.4"]
