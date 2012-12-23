e4364a5a1e0a1eb708c8d056c4629e8b287902d2
Alex Ott <alexott@gmail.com>
2012-12-23 12:49:16 +0100

    change version to 1.5.0-SNAPSHOT
    It's easier to make development in snapshot versions and use release versions separately

--- modules/incanter-sql/project.clj
+++ modules/incanter-sql/project.clj
@@ -1,11 +1,11 @@
-(defproject incanter/incanter-sql "1.4.2"
+(defproject incanter/incanter-sql "1.5.0-SNAPSHOT"
   :description "Database interaction via ClojureQL"
   :url "http://incanter.org/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git" :url "https://github.com/liebke/incanter"}
   :min-lein-version "2.0.0"
-  :dependencies [[incanter/incanter-core "1.4.2"]
+  :dependencies [[incanter/incanter-core "1.5.0-SNAPSHOT"]
                  [clojureql "1.0.4"]
                  ]
   :profiles {:dev {:dependencies [[org.apache.derby/derby "10.8.1.2"]]}}
