d2f02d05de82011f19c2ff64dc38bd434ff05b0e
Alex Ott <alexott@gmail.com>
2013-06-17 03:56:47 -0600

    bump version to 1.5.2-SNAPSHOT

--- modules/incanter-latex/project.clj
+++ modules/incanter-latex/project.clj
@@ -1,11 +1,11 @@
-(defproject incanter/incanter-latex "1.5.1"
+(defproject incanter/incanter-latex "1.5.2-SNAPSHOT"
   :description "Library for rendering LaTeX math equations using the jLateXMath library."
   :url "http://incanter.org/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git" :url "https://github.com/liebke/incanter"}
   :min-lein-version "2.0.0"
-  :dependencies [[incanter/incanter-charts "1.5.1"]
+  :dependencies [[incanter/incanter-charts "1.5.2-SNAPSHOT"]
                  [org.clojure/clojure "1.5.1"]
                  [org.scilab.forge/jlatexmath "0.9.6"]]
   )
--- modules/incanter-zoo/project.clj
+++ modules/incanter-zoo/project.clj
@@ -1,11 +1,11 @@
-(defproject incanter/incanter-zoo "1.5.1"
+(defproject incanter/incanter-zoo "1.5.2-SNAPSHOT"
   :description "Incanter Zoo is a port of Zoo from R."
   :url "http://incanter.org/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git" :url "https://github.com/liebke/incanter"}
   :min-lein-version "2.0.0"
-  :dependencies [[incanter/incanter-io "1.5.1"]
+  :dependencies [[incanter/incanter-io "1.5.2-SNAPSHOT"]
                  [org.clojure/clojure "1.5.1"]
                  [clj-time "0.5.0"
                   :exclusions [org.clojure/clojure
