82a43883a20a574c452c493bf1b01969f4fd4772
Alex Ott <alexott@gmail.com>
2013-08-13 19:09:58 +0200

    bump version to 1.5.3-SNAPSHOT

--- modules/incanter-excel/project.clj
+++ modules/incanter-excel/project.clj
@@ -1,11 +1,11 @@
-(defproject incanter/incanter-excel "1.5.2"
+(defproject incanter/incanter-excel "1.5.3-SNAPSHOT"
   :description "Incanter-excel provides access to reading and writing Excel files."
   :url "http://incanter.org/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git" :url "https://github.com/liebke/incanter"}
   :min-lein-version "2.0.0"
-  :dependencies [[incanter/incanter-core "1.5.2"]
+  :dependencies [[incanter/incanter-core "1.5.3-SNAPSHOT"]
                  [org.clojure/clojure "1.5.1"]
                  [org.apache.poi/poi-ooxml "3.9"]]
   )
--- modules/incanter-io/project.clj
+++ modules/incanter-io/project.clj
@@ -1,11 +1,11 @@
-(defproject incanter/incanter-io "1.5.2"
+(defproject incanter/incanter-io "1.5.3-SNAPSHOT"
   :description "Incanter-io is the I/O module of the Incanter project."
   :url "http://incanter.org/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git" :url "https://github.com/liebke/incanter"}
   :min-lein-version "2.0.0"
-  :dependencies [[incanter/incanter-core "1.5.2"]
+  :dependencies [[incanter/incanter-core "1.5.3-SNAPSHOT"]
                  [org.clojure/clojure "1.5.1"]
                  ;; TODO: switch to data.csv?
                  [net.sf.opencsv/opencsv "2.3"]
