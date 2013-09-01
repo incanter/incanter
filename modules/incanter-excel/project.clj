d9dbe9a7d10beaa54407897718d88b3ae26b011d
Alex Ott <alexott@gmail.com>
2013-09-01 15:57:04 +0200

    1.5.3 release

--- modules/incanter-excel/project.clj
+++ modules/incanter-excel/project.clj
@@ -1,11 +1,11 @@
-(defproject incanter/incanter-excel "1.5.3-SNAPSHOT"
+(defproject incanter/incanter-excel "1.5.3"
   :description "Incanter-excel provides access to reading and writing Excel files."
   :url "http://incanter.org/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git" :url "https://github.com/liebke/incanter"}
   :min-lein-version "2.0.0"
-  :dependencies [[incanter/incanter-core "1.5.3-SNAPSHOT"]
+  :dependencies [[incanter/incanter-core "1.5.3"]
                  [org.clojure/clojure "1.5.1"]
                  [org.apache.poi/poi-ooxml "3.9"]]
   )
--- modules/incanter-pdf/project.clj
+++ modules/incanter-pdf/project.clj
@@ -1,11 +1,11 @@
-(defproject incanter/incanter-pdf "1.5.3-SNAPSHOT"
+(defproject incanter/incanter-pdf "1.5.3"
   :description "Incanter-pdf is the PDF module of the Incanter project."
   :url "http://incanter.org/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git" :url "https://github.com/liebke/incanter"}
   :min-lein-version "2.0.0"
-  :dependencies [[incanter/incanter-charts "1.5.3-SNAPSHOT"]
+  :dependencies [[incanter/incanter-charts "1.5.3"]
                  [org.clojure/clojure "1.5.1"]
                  [com.lowagie/itext "2.1.7"]]
   )
