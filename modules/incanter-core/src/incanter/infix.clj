2522dcb44a285d0119b4976e85a686e7c08ca018
dd <uagui@student.kit.edu>
2013-10-28 01:20:56 +0100

    Change coding style for infix.clj

--- modules/incanter-core/src/incanter/infix.clj
+++ modules/incanter-core/src/incanter/infix.clj
@@ -68,55 +68,49 @@
   ([col]
      ;; loop through terms in the coluence
      (loop [idx 0
-	    col col
-	    lowest-idx nil
-	    lowest-prec @+highest-precedence+]
+      col col
+      lowest-idx nil
+      lowest-prec @+highest-precedence+]
        ;; nothing left to process
        (if (empty? col)
-	 ;; return lowest found
-	 lowest-idx
-	 ;; otherwise check if current term is lower
-	 (let [prec (get @+precedence-table+ (first col))]
-	   ;; is of lower or equal precedence
-	   (if (and prec (<= prec lowest-prec))
-	     (recur (inc idx) (rest col)
-		    idx prec)
-	     ;; is of high precedence therefore skip for now
-	     (recur (inc idx) (rest col)
-		    lowest-idx lowest-prec)))))))
+   ;; return lowest found
+   lowest-idx
+   ;; otherwise check if current term is lower
+   (let [prec (get @+precedence-table+ (first col))]
+     ;; is of lower or equal precedence
+     (if (and prec (<= prec lowest-prec))
+       (recur (inc idx) (rest col)
+        idx prec)
+       ;; is of high precedence therefore skip for now
+       (recur (inc idx) (rest col)
+        lowest-idx lowest-prec)))))))
 
 (defn- translate-op
   "Translation of symbol => symbol for binary op allows for
-user defined operators"
+  user defined operators"
   ([op]
      (get @+translation-table+ op op)))
 
 (defn infix-to-prefix
   "Convert from infix notation to prefix notation"
   ([col]
-     (cond 
+    (cond
       ;; handle term only
       (not (seq? col)) col
       ;; handle sequence containing one term (i.e. handle parens)
       (= (count col) 1) (infix-to-prefix (first col))
       ;; handle all other cases
       true (let [lowest (find-lowest-precedence col)]
-	     (if (nil? lowest) ;; nothing to split
-	       col
-	       ;; (a b c) bind a to hd, c to tl, and b to op
-	       (let [[hd [op & tl]] (split-at lowest col)]
-		 ;; recurse
-		 (list (translate-op op)
-		       (infix-to-prefix hd) 
-		       (infix-to-prefix tl))))))))
+       (if (nil? lowest) ;; nothing to split
+         col
+         ;; (a b c) bind a to hd, c to tl, and b to op
+         (let [[hd [op & tl]] (split-at lowest col)]
+     ;; recurse
+     (list (translate-op op)
+           (infix-to-prefix hd)
+           (infix-to-prefix tl))))))))
 
 (defmacro formula
   "Convert from infix notation to prefix notation"
   ([& equation]
      (infix-to-prefix equation)))
-
-
-
-
-
-
