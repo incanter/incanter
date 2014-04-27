fbbdf681b2f7be9ec59492a031142ad594b4cdcf
Alex Ott <alexott@gmail.com>
2014-04-27 18:50:46 +0200

    bump version to 1.5.6-SNAPSHOT

--- modules/incanter-svg/project.clj
+++ modules/incanter-svg/project.clj
@@ -1,11 +1,11 @@
-(defproject incanter/incanter-svg "1.5.5"
+(defproject incanter/incanter-svg "1.5.6-SNAPSHOT"
   :description "Incanter-svg is the SVG module of the Incanter project."
   :url "http://incanter.org/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git" :url "https://github.com/liebke/incanter"}
   :min-lein-version "2.0.0"
-  :dependencies [[incanter/incanter-charts "1.5.5"]
+  :dependencies [[incanter/incanter-charts "1.5.6-SNAPSHOT"]
                  [org.clojure/clojure "1.5.1"]
                   [org.apache.xmlgraphics/batik-dom "1.7"]
                   [org.apache.xmlgraphics/batik-svggen "1.7"]
