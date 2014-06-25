4a863737eb9f02727f027b6000113b949ad675b6
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2014-06-25 22:27:38 +0200

    compliance tests for core.interp added

--- modules/incanter-core/test/incanter/interp/linear_tests.clj
+++ modules/incanter-core/test/incanter/interp/linear_tests.clj
@@ -1,10 +1,13 @@
 (ns incanter.interp.linear-tests
   (:use clojure.test
+        incanter.core
         incanter.interp.linear
         incanter.interp.test-common))
 
-(deftest interpolate-test
-  (test-interpolate interpolate {}))
 
-(deftest interpolate-grid-test
-  (test-interpolate-grid interpolate-grid {}))
+(deftest compliance-test
+  (doseq [impl [:clatrix :ndarray :persistent-vector :vectorz]]
+    (set-current-implementation impl)
+    (println (str "compliance test " impl))
+    (test-interpolate interpolate {})
+    (test-interpolate-grid interpolate-grid {})))
