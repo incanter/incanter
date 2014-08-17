200279565c0fb5c8a0ed8c988bcdfb4eff5feb1c
Alex Ott <alexott@gmail.com>
2014-08-17 16:23:04 +0200

    bump version to 1.9.0-SNAPSHOT

--- modules/incanter-charts/project.clj
+++ modules/incanter-charts/project.clj
@@ -1,11 +1,11 @@
-(defproject incanter/incanter-charts "1.5.5"
+(defproject incanter/incanter-charts "1.9.0-SNAPSHOT"
   :description "Incanter-charts is the JFreeChart module of the Incanter project."
   :url "http://incanter.org/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git" :url "https://github.com/liebke/incanter"}
   :min-lein-version "2.0.0"
-  :dependencies [[incanter/incanter-io "1.5.5"]
+  :dependencies [[incanter/incanter-io "1.9.0-SNAPSHOT"]
                  [incanter/jfreechart "1.0.13-no-gnujaxp"]
                  [clj-time "0.6.0" :exclusions [org.clojure/clojure]]]
   )
