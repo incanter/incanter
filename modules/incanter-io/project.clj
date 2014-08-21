a0a79e6b9b2f0d46aebaad5a60fac969c551d923
Daniel Alberto Cañas <daniel_76@yahoo.com>
2014-08-21 17:10:45 -0600

    Remove unused json dependencies.

--- modules/incanter-io/project.clj
+++ modules/incanter-io/project.clj
@@ -9,9 +9,4 @@
   :min-lein-version "2.0.0"
   :dependencies [[incanter/incanter-core "1.9.0-SNAPSHOT"]
                  [org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.csv "0.1.2"]
-                 ;; TODO: switch to data.json?
-                 [org.danlarkin/clojure-json "1.1"
-                  :exclusions [org.clojure/clojure
-                               org.clojure/clojure-contrib]]]
-  )
+                 [org.clojure/data.csv "0.1.2"]])
