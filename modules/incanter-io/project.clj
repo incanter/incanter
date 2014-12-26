c45dad0e508681f16074c8bff69cb76921edbdcd
Alex Ott <alexott@gmail.com>
2014-12-26 18:15:22 +0100

    1.5.6 release

--- modules/incanter-io/project.clj
+++ modules/incanter-io/project.clj
@@ -1,4 +1,4 @@
-(defproject incanter/incanter-io "1.5.6-SNAPSHOT"
+(defproject incanter/incanter-io "1.5.6"
   :description "Incanter-io is the I/O module of the Incanter project."
   :url "http://incanter.org/"
   :license {:name "Eclipse Public License"
@@ -7,7 +7,7 @@
         :url "https://github.com/incanter/incanter"
         :dir "modules/incanter-io"}
   :min-lein-version "2.0.0"
-  :dependencies [[incanter/incanter-core "1.5.6-SNAPSHOT"]
+  :dependencies [[incanter/incanter-core "1.5.6"]
                  [org.clojure/clojure "1.5.1"]
                  ;; TODO: switch to data.csv?
                  [net.sf.opencsv/opencsv "2.3"]
