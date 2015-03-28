6c456409a20137767a40bcd12d7027eb0a2cf2e5
Nils Blum-Oeste <nblumoeste@googlemail.com>
2015-03-28 15:42:41 +0100

    Fix covariance, correlation and PCA for matrices
    Also fixes the corresponding tests

--- modules/incanter-core/src/incanter/stats.clj
+++ modules/incanter-core/src/incanter/stats.clj
@@ -1533,11 +1533,10 @@
         (DoubleArrayList. (double-array xx))
         (DoubleArrayList. (double-array yy)))))
   ([mat]
-   (let [n (ncol mat)
-         m (nrow mat)]
+   (let [n (ncol mat)]
       (matrix
-        (for [i (range m) j (range m)]
-          (covariance (sel mat :rows i) (sel mat :rows j))) n))))
+        (for [i (range n) j (range n)]
+          (covariance (sel mat :cols i) (sel mat :cols j))) n))))
 
 
 
@@ -1582,10 +1581,10 @@
           (DoubleArrayList. (double-array yy)) (sd y))
         0.0)))
   ([mat]
-   (let [trans-mat (trans mat)
-         cov-mat (covariance trans-mat)]
-     (div cov-mat
-       (sqrt (mult (diag cov-mat) (trans (diag cov-mat))))))))
+   (let [n (ncol mat)]
+      (matrix
+        (for [i (range n) j (range n)]
+          (correlation (sel mat :cols i) (sel mat :cols j))) n))))
 
 (defn auto-correlation
   "
@@ -2725,12 +2724,11 @@
     http://en.wikipedia.org/wiki/Principal_component_analysis
   "
   ([x & options]
-    (let [svd (decomp-svd (correlation x))
-          rotation (:V* svd)
-          std-dev (sqrt (:S svd))]
-      {:std-dev std-dev
-       :rotation rotation})))
-
+   (let [svd (decomp-svd (covariance x))
+         rotation        (:V* svd)
+         std-dev         (sqrt (:S svd))]
+     {:std-dev std-dev
+      :rotation rotation})))
 
 
 (defn detabulate
