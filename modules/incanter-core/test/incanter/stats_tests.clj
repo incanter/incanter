caee4dc4a3e154ee9df5184110cd2ae90016c64b
Nils Blum-Oeste <mail@nils-blum-oeste.net>
2015-06-16 12:39:12 +0200

    Format correlation matrix test for readability

--- modules/incanter-core/test/incanter/stats_tests.clj
+++ modules/incanter-core/test/incanter/stats_tests.clj
@@ -204,8 +204,12 @@
 		 statistics)))))
 
 (defn correlation-matrix []
-  (let [mat [[1 2 3] [1 1 3] [2 1 4]]]
-    (is (m/equals [[1 -0.5 1] [-0.5 1 -0.5] [1 -0.5 1]]
+  (let [mat [[1 2 3]
+             [1 1 3]
+             [2 1 4]]]
+    (is (m/equals [[1 -0.5 1]
+                   [-0.5 1 -0.5]
+                   [1 -0.5 1]]
           (correlation mat)
           1E-7))))
 
