5ca475ad024ed9d4a8b58b9f5c1916f673292f15
mikera <mike.r.anderson.13@gmail.com>
2016-05-30 15:55:47 +0800

    Update for 1.9.1 release

--- modules/incanter-excel/project.clj
+++ modules/incanter-excel/project.clj
@@ -1,4 +1,4 @@
-(defproject incanter/incanter-excel "1.9.1-SNAPSHOT"
+(defproject incanter/incanter-excel "1.9.1"
   :description "Incanter-excel provides access to reading and writing Excel files."
   :url "http://incanter.org/"
   :license {:name "Eclipse Public License"
@@ -7,6 +7,6 @@
         :url "https://github.com/incanter/incanter"
         :dir "modules/incanter-excel"}
   :min-lein-version "2.0.0"
-  :dependencies [[incanter/incanter-core "1.9.1-SNAPSHOT"]
+  :dependencies [[incanter/incanter-core "1.9.1"]
                  [org.apache.poi/poi-ooxml "3.14"]]
   )
--- modules/incanter-mongodb/project.clj
+++ modules/incanter-mongodb/project.clj
@@ -1,4 +1,4 @@
-(defproject incanter/incanter-mongodb "1.9.1-SNAPSHOT"
+(defproject incanter/incanter-mongodb "1.9.1"
   :description "Incanter-mongodb is the MongoDB module of the Incanter project."
   :url "http://incanter.org/"
   :license {:name "Eclipse Public License"
@@ -7,7 +7,7 @@
         :url "https://github.com/incanter/incanter"
         :dir "modules/incanter-mongodb"}
   :min-lein-version "2.0.0"
-  :dependencies [[incanter/incanter-core "1.9.1-SNAPSHOT"]
+  :dependencies [[incanter/incanter-core "1.9.1"]
                  [congomongo "0.3.3"
                   :exclusions [org.clojure/clojure
                                org.clojure/clojure-contrib]]]
